f84c620654d07986cbfb13fd7100cc44dae07eb9
Juan Fernando Ramirez <ep.juan@gmail.com>
2021-11-12 14:19:21 -0500

    Creacion de validaciones en funcion register

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,23 @@
 const User = require('../models/User');
+const { registerValidation } = require('../validations/validation');
 
 exports.register = async (req, res) => {
 
+    const {error} = registerValidation(req.body);
+    
+    if (error) {
+        return res.status(400).json(
+            {error: error.details[0].message}
+        )
+    }
+    
     const { name, email, password} = req.body;
+
+    // Validar Email Unico
+    const isEmailExists = await User.findOne({email: email})
+    
+    if(isEmailExists) return res.status(400).json({error: true, message: `El Email ${email} ya ha sido registrado`})
+
     
     const user = new User({
         name,
---
+++ validations/validation.js
@@ -0,0 +1,13 @@
+const Joi = require('@hapi/joi');
+
+const registerValidation = (data) => {
+    const schemaRegister = Joi.object({
+        name: Joi.string().min(6).max(255).required(),
+        email: Joi.string().min(6).max(255).required().email(),
+        password: Joi.string().min(6).max(1024).required()
+    })
+
+    return schemaRegister.validate(data);
+}
+
+module.exports = { registerValidation }
