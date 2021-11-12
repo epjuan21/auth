a9b2e3e5b1903999a441321f6ecd604788cbfa84
Juan Fernando Ramirez <ep.juan@gmail.com>
2021-11-12 15:14:50 -0500

    Creacion ruta y funcion login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,18 +1,14 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
-const { encryptPassword } = require('../lib/encryptPassword');
-const { registerValidation } = require('../validations/validation');
+const { encryptPassword, comparePassword } = require('../lib/cryptoUtils');
+const { registerValidation, loginValidation } = require('../validations/validation');
 
-exports.register = async (req, res) => {
+exports.register = async(req, res) => {
 
     const {error} = registerValidation(req.body);
     
-    if (error) {
-        return res.status(400).json(
-            {error: error.details[0].message}
-        )
-    }
-    
+    if (error) return res.status(400).json({error: error.details[0].message})
+
     const { name, email, password} = req.body;
 
     // Validar Email Unico
@@ -38,4 +34,32 @@ exports.register = async (req, res) => {
     } catch (error) {
         res.status(400).json(error)
     }
+}
+
+exports.login = async(req, res) => {
+
+    const { error } = loginValidation(req.body)
+
+    if (error) return res.status(400).json({ error: error.details[0].message });
+    
+    const { name, email, password} = req.body;
+
+    try {
+        
+        const user = await User.findOne({ email: email})
+    
+        if(!user) return res.status(400).json({ error: true, message: 'Usuario no encontrado' });
+    
+        const validPassword = await comparePassword(password, user.password);
+    
+        if(!validPassword) return res.status(400).json({ error: 'contraseña incorrecta' })
+
+        res.status(200).json({
+            error: null,
+            message: 'Bienvenido'
+        })
+    } catch (error) {
+        res.status(400).json(error)
+    }
+
 }
--- validations/validation.js
+++ validations/validation.js
@@ -10,4 +10,14 @@ const registerValidation = (data) => {
     return schemaRegister.validate(data);
 }
 
-module.exports = { registerValidation }
+const loginValidation = (data) => {
+    const schemaRegister = Joi.object({
+        email: Joi.string().min(6).max(255).required().email(),
+        password: Joi.string().min(6).max(1024).required()
+    })
+
+    return schemaRegister.validate(data);
+    
+}
+
+module.exports = { registerValidation, loginValidation  }
